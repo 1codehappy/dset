d9794e0e00032f8372a2f2f46c2e9c2226d2467e
1giba <olamundo@gmail.com>
2021-05-16 16:46:00 -0300

    Fix nginx

--- php/configs/start.sh
+++ php/configs/start.sh
@@ -2,8 +2,6 @@
 
 if [ -f /run/nginx/nginx.pid ]; then
   nohup /usr/local/sbin/php-fpm -y \
-    /usr/local/etc/php-fpm.conf -F -O > /dev/null 2>&1 \
-  && nginx -g "daemon off;";
-else
-  /bin/bash;
+    /usr/local/etc/php-fpm.conf -F -O > /dev/null 2>&1 & \
+  nginx -g "daemon off;"
 fi
